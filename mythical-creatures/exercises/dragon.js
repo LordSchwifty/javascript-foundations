13d1a8caffd9bf62591ba70a66b7ea492bfe2485
Tom Doder <tdoder89@gmail.com>
2022-12-15 09:53:02 -0600

    feat work on unicorn and partial work on dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+ constructor(dragonName, dragonRider, isHungry = true) {
+    this.name = dragonName,
+    this.rider = dragonRider,
+    this.hungry = isHungry,
+    this.full = 0
+    
+ }
+ greet() {
+    return `Hi, ${this.rider}!`
+ }
+ eat() {
+    this.full ++
+    if(this.full >= 3) {
+        this.hungry === false
+   
+    }
 
+ }
+}
+module.exports = Dragon;
