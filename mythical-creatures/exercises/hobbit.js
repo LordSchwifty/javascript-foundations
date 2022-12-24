1655a65d35504ef8b493692fc9c09eb41e027596
Tom Doder <tdoder89@gmail.com>
2022-12-24 13:25:37 -0500

    feat finishing touches on hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,18 +1,27 @@
 class Hobbit {
-    constructor(hobbit, age = 0, adult = false) {
+    constructor(hobbit, age = 0, adult = false, old = false, hasRing = false) {
         this.name = hobbit.name
         this.age = age
         this.adult = adult
+        this.old = old
+        this.hasRing = hasRing
     }
     celebrateBirthday() {
         this.age ++
-    }
-    timeTravel(num) {
-        this.age += num
-        if(this.age > 32) {
-        this.adult = true
-    } else if (this.age )
+      if(this.age >= 101) {
+        this.old = true
+    } else if(this.age >= 33){
+            this.adult = true
     }
 }
-
+    getRing() {
+        if(this.name === 'Frodo') {
+            this.hasRing = true
+            return `Here is the ring!`
+        } else {
+            return `You can\'t have it!`
+        }
+    }
+  
+}
 module.exports = Hobbit;
